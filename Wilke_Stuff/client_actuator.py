2b1d1af621da48918199625fcfb0bcbdd6955a7c
wgrosche <wilkegrosche@gmail.com>
2020-11-07 22:51:53 +0100

    wilke is pushy

---
+++ Wilke_Stuff/client_actuator.py
@@ -0,0 +1,15 @@
+import asyncio
+import websockets
+
+
+
+async def actuator():
+    uri = "ws://localhost:8765"
+    async with websockets.connect(uri) as websocket:
+        name = input('Input Actuator name: ')
+        type = input('Actuator type: ')
+        mes = '2' + ' ' + name + ' ' + type
+        await websocket.send(mes)
+
+
+asyncio.get_event_loop().run_until_complete(actuator())
---
+++ Wilke_Stuff/server_test.py
@@ -0,0 +1,50 @@
+import asyncio
+import websockets
+import ruleset as rules
+import config
+import importlib
+import wilke_ruleset as wrs
+
+async def rule_engine(websocket, path):
+
+    #config.init_sensors()
+    sensors = []
+    actuators = []
+    server = wrs.Server()
+    while True:
+        for i in range(20):
+            try:
+                data = await websocket.recv()
+                data = data.split(' ') # First entry: Sensor name, Second Entry: Value
+
+                if data[0]=='0':
+                    # now i want to update the sensor:
+                    eval(data[1]).value = float(data[2])
+                elif data[0]=='1':
+                    server.ServerAddDevices(data)
+                    #eval(data[1]) = config.eval(data[2])(name = data[1])
+                    # exec("%s = %d" % (data[1],config.eval(data[2])(name = data[1])))
+                    # importlib.reload(config)
+                    # sensors.append(data[1])
+                    print(sensors)
+                elif data[0]=='2':
+                    #eval(data[1]) = config.eval(data[2])(name = data[1])
+                    exec("%s = %d" % (data[1],config.eval(data[2])(name = data[1])))
+                    importlib.reload(config)
+                    actuators.append(data[1])
+                    print(actuators)
+
+                await asyncio.sleep(0.2)
+
+            except websockets.ConnectionClosed:
+                #print(f"Terminated")
+                break
+
+        #logic phase
+        config.updates()
+
+
+start_server = websockets.serve(rule_engine, "localhost", 8765)
+
+asyncio.get_event_loop().run_until_complete(start_server)
+asyncio.get_event_loop().run_forever()
