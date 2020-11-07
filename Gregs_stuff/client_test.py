98a6e20f5ba056c2dc5ecc27ca93198150fd2024
sunnyboy30 <74073756+sunnyboy30@users.noreply.github.com>
2020-11-07 20:36:47 +0100

    fixes
    I made it possible to add sensors and rules while the server is running

--- Gregs_stuff/client_test.py
+++ Gregs_stuff/client_test.py
@@ -6,8 +6,14 @@ import websockets
 async def sensor():
     uri = "ws://localhost:8765"
     async with websockets.connect(uri) as websocket:
+        name = input('Input Sensor name: ')
+        type = input('Sensor Sensor type: ')
+        await websocket.send(1,name,type)
         while True:
-            value = 'Sensor_name,1'
+            value = '0,Sensor_name,1'
+            #maybe i could do a measurement here
+            # this could for example be done by connecting to another server
+            # that simulates movement in the city
             await websocket.send(value)
 
 
